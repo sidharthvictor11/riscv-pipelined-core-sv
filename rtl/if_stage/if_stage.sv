a9773ee9ba30ee86f1890b0cd9866bf3c015ba1e
Sidharth T <tsidharth1105@gmail.com>
2026-07-19 22:43:32 +0530

    Update Instruction Fetch Module

--- rtl/if_stage/if_stage.sv
+++ rtl/if_stage/if_stage.sv
@@ -1,45 +1,27 @@
-module if_stage #(
-  parameter int XLEN = 32,
-  parameter logic [XLEN -1:0] RESET_PC = '0
-)(
-  input logic i_clk,
-  input logic i_rst_n,
-  
-  input logic i_branch_taken,
-  input logic [XLEN-1:0] i_branch_addr,
-  
-  output logic [XLEN-1:0] o_imem_addr,
-  input logic [31:0] i_imem_rdata,
-  
-  output logic [XLEN-1:0] o_if_pc,
-  output logic [31:0] o_if_instr,
-  output logic o_if_valid
+module fetch (
+    input logic clk,
+    input logic reset_n,
+    
+    input logic [31:0] pc,
+    
+    output logic imem_req,
+    output logic [31:0] imem_addr,
+    
+    input logic [31:0] imem_data,
+    output logic [31:0] instruction
 );
 
-    logic [XLEN-1:0] pc_d, pc_q;
-    
-    //next program counter logic
-    always_comb begin
-      if(i_branch_taken)
-        pc_d = i_branch_addr;
-      else
-        pc_d = pc_q + XLEN'(4);
-    end
-    
-    //pc register
-    always_ff @(posedge i_clk) begin
-      if(!i_rst_n)
-        pc_q <= RESET_PC;
-      else
-        pc_q <= pc_d;
-    end
-    
-    //Instruction memory interface
-    assign o_imem_addr = pc_q;
-    
-    //Output to Decode
-    assign o_if_pc = pc_q;
-    assign o_if_instr = i_imem_rdata;
-    assign o_if_valid = 1'b1;
+logic req_reg;
+
+always_ff @(posedge clk or negedge reset_n) begin
+    if(!reset_n) 
+        req_reg <= 1'b0;
+    else
+        req_reg <= 1'b1;
+end
+
+assign imem_req  = req_reg;
+assign imem_addr = pc;
+assign instruction = imem_data;
 
 endmodule
